01415073cdb24735d4de22e16801af6fd9997643
Pratyay <praty94@gmail.com>
2019-08-08 13:18:17 -0400

    updated create page

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,8 @@
+/*constants*/
+var homeTemplate = 'home-template';
+var createPageTemplate = "create-template";
+var _linksInStorage = null;
+
 $(document).ready(function () {
     renderAllSites();
     $('#addPage').on('click', function () {
@@ -7,15 +12,25 @@ $(document).ready(function () {
         }, function (tabs) {
             var tab = tabs[0];
             if (tab.url) {
-                //saving to storage
-                saveLink(tab.url, 0);
+                /* var url = new URL(tab.url);
+                 var linkObj = {
+                     url:tab.url,
+                     name:"defaultname",
+                     hostname:url.hostname
+                 }
+                 //saving to storage
+                 saveLink(linkObj, 0);*/
+                renderCreatePage(tab.url);
             } else {
                 alert('Please wait for the page to load!');
             }
+
         });
     });
-    
+
 });
+
+/*data generation functions <start>*/
 function getDefaultLinkArray() {
     var curLinks =
         [
@@ -49,6 +64,10 @@ function getViewModel(curLinks, curGroup) {
     }
     return viewModel;
 }
+
+/*data generation functions <end>*/
+
+/*render functions*/
 function renderAllSites() {
     chrome.storage.sync.get(["links"], function (links) {
         var curLinks = links.links;
@@ -56,45 +75,77 @@ function renderAllSites() {
         if (!curLinks) {
             curLinks = getDefaultLinkArray();
         }
-
-        handlebarsCompile(getViewModel(curLinks));
+        _linksInStorage = curLinks;
+        handlebarsCompile(getViewModel(curLinks), homeTemplate);
     });
 }
-function changeGroupSelection(id){
+
+function renderCreatePage(url) {
     chrome.storage.sync.get(["links"], function (links) {
         var curLinks = links.links;
 
         if (!curLinks) {
             curLinks = getDefaultLinkArray();
         }
+        
+        //Saving to be used later
+        _linksInStorage = curLinks;
 
-        handlebarsCompile(getViewModel(curLinks,id));
+        var context = getViewModel(curLinks);
+        context.url = url;//additionally adding url to view model
+        handlebarsCompile(context, createPageTemplate);
     });
 }
-function handlebarsCompile(context) {
-    var source = $('#entry-template').html();
+
+function changeGroupSelection(id) {
+    var curLinks = _linksInStorage;
+
+    if (!curLinks) {
+        curLinks = getDefaultLinkArray();
+    }
+
+    handlebarsCompile(getViewModel(curLinks, id), homeTemplate);
+}
+function changeSelectionCreatePage(id){
+    var curLinks = _linksInStorage;
+
+    if (!curLinks) {
+        curLinks = getDefaultLinkArray();
+    }
+    var context = getViewModel(curLinks, id);
+    context.name = $('#page-name').val();
+    context.url = $('#page-url').val();
+    handlebarsCompile(context, createPageTemplate);
+}
+function handlebarsCompile(context, templateId) {
+    var source = $("#"+templateId).html();
     var template = Handlebars.compile(source);
 
     var html = template(context);
     $('#container').html(html);
-    
+
     //registering events after recompiling template
     registerEvents();
 }
-function registerEvents(){
+function registerEvents() {
     $('.group-item').on('click', function () {
         var id = $(this).attr('data-id');
-        changeGroupSelection(id);
+        var parent = $(this).attr('data-parent');
+        if (parent == homeTemplate) {
+            changeGroupSelection(id);
+        } else if (parent == createPageTemplate) {
+            changeSelectionCreatePage(id);
+        }
     });
 }
-function saveLink(url, group) {
+function saveLink(linkObj, group) {
     chrome.storage.sync.get(["links"], function (links) {
         var curLinks = links.links;
         if (!curLinks) {
             curLinks = getDefaultLinkArray();
         }
         if (curLinks[group] && curLinks[group].links) {
-            curLinks[group].links.push(url);
+            curLinks[group].links.push(linkObj);
             saveToStorage({ links: curLinks });
             renderAllSites();
         }
